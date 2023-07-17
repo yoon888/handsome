c89c11bd666c62983597c8f711e9bf493c6fecd5
yoon88 <ysmdudwo@hotmail.com>
2023-07-17 17:13:30 +0900

    web

--- js/common.js
+++ js/common.js
@@ -1,31 +1,51 @@
-$(function(){
-    $('.header').load('../inc/header.html')
-    $('.footer').load('../inc/footer.html')
+$(function () {
+  $(".header").load("../inc/header.html");
+  $(".footer").load("../inc/footer.html");
 
-    $('.main_slide_01').slick({
-      autoplay : true,
-      slidesToShow: 4,
-      autoplaySpeed: 3000,
-      responsive: [
-        {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 2,
-        }
-      },
-      ]
+  $(".slide-image").slick({
+    dots: false,
+    items: 5,
+    cssClass: "slick-dots",
+    nextArrow: $(".right-arrow"),
+    prevArrow: $(".left-arrow"),
+    autoplay: true,
+    autoplaySpeed: 5000,
+  });
+
+  // $(".page").fullpage({
+  //   //options here
+  //   autoScrolling: true,
+  //   scrollHorizontally: true,
+  //   menu: false,
+  //   navigation: true,
+  //   responsive: {
+  //     breakpoints: [
+  //       {
+  //         width: 768,
+  //         sections: ['.item01','.item02']
+  //       },
+  //     ]
+  // });
+
+  $(".bottom-items").slick({
+    infinite: true,
+    slidesToShow: 6,
+    slidesToScroll: 6,
+  });
+
+  $(".mbn-slide").slick({
+    infinite: true,
+    slidesToShow: 6,
+    slidesToScroll: 6,
+    arrows: false,
+    dots: true,
+  });
+
+  $(".mbn-image").mouseover(function () {
+    $(".mbn-image .text").show();
+
+    $(".mbn-image").mouseleave(function () {
+      $(".mbn-image .text").css("display", "none");
     });
-    $('.main_slide_02').slick({
-      autoplay : true,
-      slidesToShow: 4,
-      autoplaySpeed: 2000,
-      responsive: [
-        {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 2,
-        }
-      },
-      ]
-    });
-})
+  });
+});
